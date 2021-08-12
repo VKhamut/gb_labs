2ea6e19daaa8947a7abc0d029be4c76c7e00db47
VKhamut <vkhamut@outlook.com>
2021-08-12 12:17:13 +0700

    lab_1 upload

---
+++ lab_1/task_1.py
@@ -0,0 +1,12 @@
+#1. Поработайте с переменными, создайте несколько, выведите на экран, запросите у пользователя несколько чисел и строк и сохраните в переменные, выведите на экран.
+print("\ntask_1\n")
+var1 = int(input("Enter digit: "))
+print(var1)
+
+var2 = str(input("Enter string: "))
+print(var2)
+
+my_list = list(['1', '2', '4', '5'])
+print(my_list)
+my_list.append(input("Enter something for adding to list "))
+print(my_list)
