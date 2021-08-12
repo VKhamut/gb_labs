68b8bb1162ed4a50eb47f7d5c38d8f85abe6ad74
VKhamut <88814699+VKhamut@users.noreply.github.com>
2021-08-12 16:20:02 +0700

    Delete lab_2 directory

--- lab_2/task_3.py
+++
@@ -1,4 +0,0 @@
-# 3. Пользователь вводит месяц в виде целого числа от 1 до 12. Сообщить к какому времени года относится месяц (зима, весна, лето, осень). Напишите решения через list и через dict.
-month = int(input("Enter number of month (1-12): "))
-month_list = ['winter', 'winter', 'spring', 'spring', 'spring', 'summer', 'summer', 'summer', 'autmun', 'autmun', 'autmun', 'winter']
-print("This is ", month_list[month - 1])
