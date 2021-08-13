1fbe5455652367c66061626c140f7e52d55ebdfe
VKhamut <88814699+VKhamut@users.noreply.github.com>
2021-08-13 18:12:08 +0700

    Delete lab_3 directory

--- lab_3/task_3.py
+++
@@ -1,7 +0,0 @@
-# 3. Реализовать функцию my_func(), которая принимает три позиционных аргумента, и возвращает сумму наибольших двух аргументов.
-
-def my_func(num_1, num_2, num_3):
-   tmp_list = sorted([num_1, num_2, num_3])
-   return(tmp_list[-1] + tmp_list[-2])
-    
-print(my_func(8, 3, 6))
