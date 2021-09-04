d0680de97aaf1a422372f87c2d63b22b33ef5ca6
VKhamut <88814699+VKhamut@users.noreply.github.com>
2021-09-04 11:40:58 +0700

    Delete lab_5 directory

--- lab_5/task_1.py
+++
@@ -1,7 +0,0 @@
-# 1. Создать программно файл в текстовом формате, записать в него построчно данные, вводимые пользователем. Об окончании ввода данных свидетельствует пустая строка.
-
-with open("./task_1_file.txt", "w") as f:
-    while True:
-        new_line = input("Enter new line or press Enter to finish: ")
-        if new_line == "": break
-        print(new_line, file=f)
